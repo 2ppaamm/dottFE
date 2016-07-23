65be5a9a7e9406aa581deb93fb659658a643a632
Pamela Lim <pamelaliusm@gmail.com>
2016-07-23 17:12:22 +1000

    responsive

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'outerBounds': {
-      'width': 800,
+      'width': 700,
       'height': 1200
     }
   });
--- js/quiz.js
+++ js/quiz.js
@@ -1,11 +1,21 @@
 (function(){
-
-	var	app = angular.module('mathQuiz', []);
-	app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+	var	app = angular.module('mathQuiz', ['auth0', 'angular-storage', 'angular-jwt', 'ngRoute']);
+	app.controller('QuizController', ['$scope', '$http', '$sce', 'auth', 'store', function($scope, $http, $sce, auth, store){
 		$scope.score = 0;
 		$scope.activeQuestion = -1;
 		$scope.activeQuestionAnswered = 0;
 		$scope.percentage = 0;
+
+		$scope.login = function(){
+			$scope.activeQuestion = 0;
+		    // Set popup to true to use popup
+		    auth.signin({popup: true}, function(profile, token){
+		      store.set('profile', profile);
+		      store.set('token', token);
+		    }, function(err){
+		      console.log('unable to signin through Auth0');
+		    });
+		}
 
 		$http.get('quiz_data.json').then(function(quizData){
 			$scope.myQuestions = quizData.data;
@@ -50,4 +60,52 @@
 
 	}]);
 
+	app.config( function myAppConfig (authProvider) {
+		authProvider.init({
+		    domain: 'pamelalim.auth0.com',
+		    clientID: 'eVJv6UFM9GVdukBWiURczRCxmb6iaUYG'
+		});
+		authProvider.on('loginSuccess', ['$location', 'profilePromise', 'idToken', 'store', function($location, profilePromise, idToken, store) {
+		  // Successfully log in
+		  // Access to user profile and token
+		  profilePromise.then(function(profile){
+		    // profile
+		    store.set('profile', profile);
+		    store.set('token', token);
+		  });
+		  $location.url('/');
+		}]);
+		//Called when login fails
+		authProvider.on('loginFailure', function() {
+		  console.log('login failed');
+		});
+	});
+
+	app.run(['$rootScope', 'auth', 'store', 'jwtHelper', '$location', function($rootScope, auth, store, jwtHelper, $location) {
+	  // Listen to a location change event
+	  $rootScope.$on('$locationChangeStart', function() {
+	    // Grab the user's token
+	    var token = store.get('token');
+	    // Check if token was actually stored
+	    if (token) {
+	      // Check if token is yet to expire
+	      if (!jwtHelper.isTokenExpired(token)) {
+	        // Check if the user is not authenticated
+	        if (!auth.isAuthenticated) {
+	          // Re-authenticate with the user's profile
+	          // Calls authProvider.on('authenticated')
+	          auth.authenticate(store.get('profile'), token);
+	        }
+	      } else {
+	        // Either show the login page
+	        // $location.path('/');
+	        // .. or
+	        // or use the refresh token to get a new idToken
+	        auth.refreshIdToken(token);
+	      }
+	    }
+
+	  });
+	}]);
+	
 })();
